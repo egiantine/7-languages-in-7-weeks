ebde86170626d9972777723e07ad81b199f113ff
pork <pork@users.noreply.github.com>
2014-04-09 14:13:00 -0400

    prolog day 1

---
+++ 3week-prolog/day1.pl
@@ -0,0 +1,19 @@
+% Books
+wrote(war_and_peace, tolstoy).
+wrote(anna_karenina, tolstoy).
+wrote(the_brothers_karamazov, dostoevsky).
+wrote(crime_and_punishment, dostoevsky).
+wrote(on_the_eve, turgenev).
+
+% Now we can answer the question:
+% wrote(What, tolstoy).
+
+% Musicians
+played(richter, piano).
+played(richard_lloyd, guitar).
+
+genre(richter, classical).
+genre(richard_lloyd, rock).
+
+% Now we can answer the question:
+% played(What, guitar).
